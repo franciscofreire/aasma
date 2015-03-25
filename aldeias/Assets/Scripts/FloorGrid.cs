ef276f90d6e5ef23a7824b94199dc73823b7cc88
pafalium <pedroalfaiate1992@hotmail.com>
2015-03-25 17:10:51 +0000

    Initial import

---
+++ aldeias/Assets/Scripts/FloorGrid.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FloorGrid : MonoBehaviour {
+
+	public FloorTile tilePrefab;
+	public int rows = 50;
+	public int columns = 50;
+
+	private List<List<FloorTile>> tileMatrix;
+
+	// Use this for initialization
+	void Start () {
+		initializeTileMatrix();
+	}
+
+	void initializeTileMatrix() {
+		tileMatrix = new List<List<FloorTile>>(rows);
+
+		Quaternion tileRotation = Quaternion.AngleAxis(90, Vector3.right);
+		for(int row=0; row < rows; row++) {
+			List<FloorTile> rowList = new List<FloorTile>(columns);
+			tileMatrix.Add(rowList);
+			for(int column=0; column < columns; column++) {
+				GameObject tile = (GameObject) Object.Instantiate(tilePrefab.gameObject, Vector3.right * row + Vector3.forward * column, tileRotation);
+				tile.transform.parent = transform;
+				FloorTile floorTile = tile.GetComponent<FloorTile>();
+				floorTile.initTile(row, column);
+				rowList.Add(floorTile);
+			}
+		}
+	}
+}
---
+++ aldeias/Assets/Scripts/FloorTile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorTile : MonoBehaviour {
+	
+	public int row;
+	public int column;
+
+	public void initTile (int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+}
