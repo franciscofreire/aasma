3713c875e619bf78a29dee9497123878f75fbb2f
pafalium <pedroalfaiate1992@hotmail.com>
2015-04-15 10:38:06 +0100

    Changed FloorGrid to have a separate quad for every tile. Each tile maps to a part of a texture atlas.

--- aldeias/Assets/Scripts/FloorGrid.cs
+++ aldeias/Assets/Scripts/FloorGrid.cs
@@ -13,98 +13,127 @@ public class FloorGrid : MonoBehaviour {
 	
 	public Texture2D terrainTiles;
 	public int tileResolution;
+
+	public Mesh floorMesh;
 	
 	// Use this for initialization
 	void Start () {
 		BuildMesh();
 	}
 	
-	Color[][] ChopUpTiles() {
+	struct TileTexInfo {
+		public Vector2[,] tileCorners; // the (0,0) corner of the tile texture
+		public Vector2 tileSquareSize;
+	}
+	TileTexInfo MakeAtlasCoordinates() {
 		int numTilesPerRow = terrainTiles.width / tileResolution;
 		int numRows = terrainTiles.height / tileResolution;
 		
-		Color[][] tiles = new Color[numTilesPerRow*numRows][];
+		Vector2[,] tileCorners = new Vector2[ numRows*numTilesPerRow, 4 ];
+		Vector2 tileSquareSize = new Vector2( tileResolution / (float) terrainTiles.width, tileResolution / (float) terrainTiles.height );
 		
 		for(int y=0; y<numRows; y++) {
 			for(int x=0; x<numTilesPerRow; x++) {
-				tiles[y*numTilesPerRow + x] = terrainTiles.GetPixels(
-					x*tileResolution,
-					y*tileResolution, 
-					tileResolution,
-					tileResolution );
+				int tileIndex = y*numTilesPerRow + x;
+				Vector2 corner_0_0 = new Vector2( tileSquareSize.x*x, tileSquareSize.y*y );
+				//(0,0)
+				tileCorners[ tileIndex, 0 ] = corner_0_0;
+				//(1,0)
+				tileCorners[ tileIndex, 1 ] = new Vector2(corner_0_0.x+tileSquareSize.x, corner_0_0.y);
+				//(1,1)
+				tileCorners[ tileIndex, 2 ] = new Vector2(corner_0_0.x+tileSquareSize.x, corner_0_0.y+tileSquareSize.y);
+				//(0,1)
+				tileCorners[ tileIndex, 3 ] = new Vector2(corner_0_0.x, corner_0_0.y+tileSquareSize.y);
 			}
 		}
 		
-		return tiles;
+		TileTexInfo res;
+		res.tileCorners = tileCorners;
+		res.tileSquareSize = tileSquareSize;
+		return res;
 	}
-	
-	void BuildTexture() {
-		DTileMap map = new DTileMap(size_x, size_z);
-		
-		int texWidth = size_x * tileResolution;
-		int texHeight = size_z * tileResolution;
-		Texture2D texture = new Texture2D(texWidth, texHeight);
-		
-		Color[][] tiles = ChopUpTiles();
-		
-		for(int y=0; y < size_z; y++) {
+
+	void SetDebugUVs(Mesh mesh) {
+		TileTexInfo atlasInfo = MakeAtlasCoordinates();
+		int atlasSize = atlasInfo.tileCorners.GetLength(0);
+
+		int curAtlasTile = 0;
+		Vector2[] newUVs = new Vector2[mesh.uv.Length];
+		for(int z=0; z < size_z; z++) {
 			for(int x=0; x < size_x; x++) {
-				Color[] p = tiles[ map.GetTileAt(x,y) ];
-				texture.SetPixels(
-					x*tileResolution,
-					y*tileResolution, 
-					tileResolution, 
-					tileResolution, 
-					p);
+				int quadIndex = z*size_x + x;
+				int quadVertexBaseIndex = quadIndex*4;
+
+				newUVs[ quadVertexBaseIndex + 0 ] = atlasInfo.tileCorners[curAtlasTile, 0];
+				newUVs[ quadVertexBaseIndex + 1 ] = atlasInfo.tileCorners[curAtlasTile, 1];
+				newUVs[ quadVertexBaseIndex + 2 ] = atlasInfo.tileCorners[curAtlasTile, 2];
+				newUVs[ quadVertexBaseIndex + 3 ] = atlasInfo.tileCorners[curAtlasTile, 3];
+
+				curAtlasTile = (curAtlasTile+1) % atlasSize;
 			}
 		}
-		
-		texture.filterMode = FilterMode.Point;
-		texture.wrapMode = TextureWrapMode.Clamp;
-		texture.Apply();
-		
-		MeshRenderer mesh_renderer = GetComponent<MeshRenderer>();
-		mesh_renderer.sharedMaterials[0].mainTexture = texture;
-		
-		Debug.Log ("Done Texture!");
+		mesh.uv = newUVs;
 	}
-	
+
 	public void BuildMesh() {
 		int numTiles = size_x * size_z;
-		int numTris = numTiles * 2;
 		
 		int vsize_x = size_x + 1;
 		int vsize_z = size_z + 1;
-		int numVerts = vsize_x * vsize_z;
+		int numQuads = numTiles;
+		int numVerts = numQuads * 4;
+		int numTris = numQuads * 2;
 		
 		// Generate the mesh data
 		Vector3[] vertices = new Vector3[ numVerts ];
-		Vector3[] normals = new Vector3[numVerts];
-		Vector2[] uv = new Vector2[numVerts];
+		Vector3[] normals = new Vector3[ numVerts ];
+		Vector2[] uv = new Vector2[ numVerts ];
 		
 		int[] triangles = new int[ numTris * 3 ];
 		
 		int x, z;
-		for(z=0; z < vsize_z; z++) {
-			for(x=0; x < vsize_x; x++) {
-				vertices[ z * vsize_x + x ] = new Vector3( x*tileSize, 0, -z*tileSize );
-				normals[ z * vsize_x + x ] = Vector3.up;
-				uv[ z * vsize_x + x ] = new Vector2( (float)x / size_x, 1f - (float)z / size_z );
+		for(z=0; z < size_z; z++) {
+			for(x=0; x < size_x; x++) {
+				int quadIndex = z*size_x + x;
+				int quadVertexBaseIndex = quadIndex*4;
+
+				//x=0,z=0
+				vertices[ quadVertexBaseIndex + 0 ] = new Vector3( x*tileSize, 0, -z*tileSize );
+				normals[ quadVertexBaseIndex + 0 ] = Vector3.up;
+				uv[ quadVertexBaseIndex + 0 ] = new Vector2( 0, 0 );
+				//x=1,z=0
+				vertices[ quadVertexBaseIndex + 1 ] = new Vector3( (x+1)*tileSize, 0, -z*tileSize );
+				normals[ quadVertexBaseIndex + 1 ] = Vector3.up;
+				uv[ quadVertexBaseIndex + 1 ] = new Vector2( 1, 0 );
+				//x=1,z=1
+				vertices[ quadVertexBaseIndex + 2 ] = new Vector3( (x+1)*tileSize, 0, -(z+1)*tileSize );
+				normals[ quadVertexBaseIndex + 2 ] = Vector3.up;
+				uv[ quadVertexBaseIndex + 2 ] = new Vector2( 1, 1 );
+				//x=0,z=1
+				vertices[ quadVertexBaseIndex + 3 ] = new Vector3( x*tileSize, 0, -(z+1)*tileSize );
+				normals[ quadVertexBaseIndex + 3 ] = Vector3.up;
+				uv[ quadVertexBaseIndex + 3 ] = new Vector2( 0, 1 );
 			}
 		}
 		Debug.Log ("Done Verts!");
 		
 		for(z=0; z < size_z; z++) {
 			for(x=0; x < size_x; x++) {
-				int squareIndex = z * size_x + x;
-				int triOffset = squareIndex * 6;
-				triangles[triOffset + 0] = z * vsize_x + x + 		   0;
-				triangles[triOffset + 2] = z * vsize_x + x + vsize_x + 0;
-				triangles[triOffset + 1] = z * vsize_x + x + vsize_x + 1;
-				
-				triangles[triOffset + 3] = z * vsize_x + x + 		   0;
-				triangles[triOffset + 5] = z * vsize_x + x + vsize_x + 1;
-				triangles[triOffset + 4] = z * vsize_x + x + 		   1;
+				int quadIndex = z*size_x + x;
+				int quadVertexBaseIndex = quadIndex*4;
+				int corner_0_0 = quadVertexBaseIndex + 0;
+				int corner_1_0 = quadVertexBaseIndex + 1;
+				int corner_1_1 = quadVertexBaseIndex + 2;
+				int corner_0_1 = quadVertexBaseIndex + 3;
+
+				int triBaseIndex = quadIndex * 6;
+				triangles[ triBaseIndex + 0 ] = corner_0_0;
+				triangles[ triBaseIndex + 1 ] = corner_1_0;
+				triangles[ triBaseIndex + 2 ] = corner_1_1;
+
+				triangles[ triBaseIndex + 3 ] = corner_1_1;
+				triangles[ triBaseIndex + 4 ] = corner_0_1;
+				triangles[ triBaseIndex + 5	 ] = corner_0_0;
 			}
 		}
 		
@@ -112,6 +141,7 @@ public class FloorGrid : MonoBehaviour {
 		
 		// Create a new Mesh and populate with the data
 		Mesh mesh = new Mesh();
+		mesh.MarkDynamic();
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.normals = normals;
@@ -120,11 +150,35 @@ public class FloorGrid : MonoBehaviour {
 		// Assign our mesh to our filter/renderer/collider
 		MeshFilter mesh_filter = GetComponent<MeshFilter>();
 		MeshCollider mesh_collider = GetComponent<MeshCollider>();
-		
+
+		floorMesh = mesh;
 		mesh_filter.mesh = mesh;
 		mesh_collider.sharedMesh = mesh;
 		Debug.Log ("Done Mesh!");
 		
-		BuildTexture();
+		//BuildTexture();
+		SetDebugUVs(mesh);
+		MeshRenderer mesh_renderer = GetComponent<MeshRenderer>();
+		mesh_renderer.sharedMaterials[0].mainTexture = terrainTiles;
 	}
+
+	//Return a function that returns the tile to be used as the tile of the (x,z) tile.
+	/*public delegate int GetTile(int x, int z);
+	void SetTiles(GetTile tileFunction) {
+		Vector2[] newUVs = new Vector2[floorMesh.uv.Length];
+		for(int z=0; z < size_z; z++) {
+			for(int x=0; x < size_x; x++) {
+				int quadIndex = z*size_x + x;
+				int quadVertexBaseIndex = quadIndex*4;
+				
+				newUVs[ quadVertexBaseIndex + 0 ] = atlasInfo.tileCorners[curAtlasTile, 0];
+				newUVs[ quadVertexBaseIndex + 1 ] = atlasInfo.tileCorners[curAtlasTile, 1];
+				newUVs[ quadVertexBaseIndex + 2 ] = atlasInfo.tileCorners[curAtlasTile, 2];
+				newUVs[ quadVertexBaseIndex + 3 ] = atlasInfo.tileCorners[curAtlasTile, 3];
+				
+				curAtlasTile = (curAtlasTile+1) % atlasSize;
+			}
+		}
+		mesh.uv = newUVs;
+	}*/
 }
