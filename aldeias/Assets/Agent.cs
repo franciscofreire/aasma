4557bf496e03e9cd898a1441d6c771bd546249f9
Francisco Freire <evolbit@gmail.com>
2015-04-15 13:03:47 +0100

    Added some attributes to agent classes

--- aldeias/Assets/Agent.cs
+++ aldeias/Assets/Agent.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public abstract class Agent {
 	public Vector2 pos;
-	public Agent () {
 
+	public Agent (Vector2 pos) {
+		this.pos = pos;
 	}
+
 	public abstract void doAction();
 }
--- aldeias/Assets/Habitant.cs
+++ aldeias/Assets/Habitant.cs
@@ -1,4 +1,12 @@
+using UnityEngine;
+
 public class Habitant : Agent {
+	public string tribeId;
+
+	public Habitant(Vector2 pos, string tribeId) {
+		Agent(pos);
+		this.tribeId = tribeId;
+	}
 	public override void doAction() {
 		//TODO
 	}
