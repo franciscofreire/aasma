38314d37640ba6eb2b48c4b7f09968b498b22769
Francisco Freire <evolbit@gmail.com>
2015-04-15 15:51:14 +0100

    Fixed some c# errors

--- aldeias/Assets/Agent.cs
+++ aldeias/Assets/Agent.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public abstract class Agent {
 	public Vector2 pos;
+	public Agent() {
 
+	}
 	public Agent (Vector2 pos) {
 		this.pos = pos;
 	}
--- aldeias/Assets/Habitant.cs
+++ aldeias/Assets/Habitant.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class Habitant : Agent {
 	public string tribeId;
 
-	public Habitant(Vector2 pos, string tribeId) {
-		Agent(pos);
+	public Habitant(Vector2 pos, string tribeId): base(pos) {
 		this.tribeId = tribeId;
 	}
 	public override void doAction() {
