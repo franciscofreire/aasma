3a76d8a1d08b11a78e6bbc7d33734b9545854d6b
Francisco Nunes <francisco.m.nunes@tecnico.ulisboa.pt>
2015-04-19 11:57:13 +0100

    git atrofios

--- aldeias/Assets/Scripts/Agents/Habitant.cs
+++ aldeias/Assets/Scripts/Agents/Habitant.cs
@@ -6,8 +6,6 @@ public class Habitant : Agent {
 	public WorldInfo.Tribe tribe;
 	public float  affinity; // 0: Complete Civil; 1: Complete Warrior
 	public bool   isLeader;
-    public bool carryingFood;
-    public bool carryingWood;
 
 	public Habitant(Vector2 pos, WorldInfo.Tribe tribe, float affinity): base(pos) {
 		this.tribe  = tribe;
@@ -16,39 +14,20 @@ public class Habitant : Agent {
 	}
 
 	public override Action doAction() {
-		/*	if (enemy-in-front? or animal-in-front?) and low-energy?
-				move();
-			else if	enemy-in-front? or animal-in-front?
-				attack();
-			else if tree-in-front?
-				crop-tree();
-            else if stump-in-front?
-                collectWood();
-            else if food-in-front?
-                collectFood();
-            else if meeting-point-in-front and carrying-resources?
-                dropResources();
-			else if unclaimed-territory-in-front?
-				place-flag()
-			else
-				move();
-		*/
-        
-        int index = worldInfo.rnd.Next(sensorData.Cells.Count);
-        
-        Vector2I target = sensorData.Cells[index];
-        
-        return new Walk(this, target);
+		//TODO
+		return null;
 	}
 
 	public override void OnWorldTick () {
 		sensorData.Cells = worldInfo.nearbyFreeCells(worldInfo.nearbyCells(this));
 
-        Action a = doAction();
-        a.apply();
+		int index = worldInfo.rnd.Next(sensorData.Cells.Count);
+		
+		Vector2I target = sensorData.Cells[index];
 
+		Action a = new Walk(this, target);
+		a.apply();
 	}
-
 
 	//*************
 	//** SENSORS **
@@ -60,37 +39,4 @@ public class Habitant : Agent {
 		Habitant habInFront = worldInfo.habitantInTile(tileCoordInFront);
 		return habInFront != null && habInFront.tribe != this.tribe;
 	}
-
-	private bool AnimalInFront() {
-		// TODO
-        return false;
-	}
-
-	private bool TreeInFront() {
-        // TODO
-        return false;
-	}
-
-    private bool StumpInFront() {
-        // TODO
-        return false;
-    }
-
-	private bool UnclaimedTerritoryInFront() {
-        // TODO
-        return false;
-	}
-
-    private bool carryingResources() {
-        return carryingFood || carryingWood;
-    }
-
-	private bool LowEnergy() {
-		return this.energy <= CRITICAL_ENERGY_LEVEL;
-	}
-
-    private bool FoodInFront() {
-        // TODO
-        return false;
-    }
 }
