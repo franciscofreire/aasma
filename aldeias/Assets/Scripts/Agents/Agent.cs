6c970638c5a26b2224f9e5e587375c33d6d631a9
pafalium <pedroalfaiate1992@hotmail.com>
2015-04-26 12:14:26 +0100

    Moved Orientation to Agent.cs

--- aldeias/Assets/Scripts/Agents/Agent.cs
+++ aldeias/Assets/Scripts/Agents/Agent.cs
@@ -136,3 +136,45 @@ public struct SensorData {
 		_front_cell = front_cell;
 	}
 }
+
+
+public enum ORIENTATION {UP=0, DOWN=180, LEFT=270, RIGHT=90};
+
+public struct Orientation {
+	private ORIENTATION orientation;
+	
+	public static implicit operator Orientation(ORIENTATION orientation) {
+		return new Orientation(orientation);
+	}
+	
+	public static Orientation FromORIENTATION(ORIENTATION orientation) {
+		return orientation;
+	}
+	
+	public Vector2 ToVector2() {//Up=(0,1), Down=(0,-1), Left=(-1,0), Right=(1,0)
+		switch (orientation) {
+		case ORIENTATION.UP:
+			return new Vector2(0f,1f);
+		case ORIENTATION.DOWN:
+			return new Vector2(0f,-1f);
+		case ORIENTATION.LEFT:
+			return new Vector2(-1f,0f);
+		case ORIENTATION.RIGHT:
+			return new Vector2(1f,0f);
+		default:
+			throw new System.Exception("Cannot convert "+orientation+" to UnityEngine.Vector2.");
+		}
+	}
+	
+	public Quaternion ToQuaternion() {
+		return Quaternion.AngleAxis((float)orientation, Vector3.up);
+	}
+	
+	public Quaternion ToQuaternionInX() {
+		return Quaternion.AngleAxis((float)orientation, Vector3.right);
+	}
+	
+	private Orientation(ORIENTATION orientation) {
+		this.orientation = orientation;
+	}
+}
