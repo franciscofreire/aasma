870d4c35cca4c579a13d1c0acb13aa567cccbdcd
Francisco Freire <evolbit@gmail.com>
2015-05-02 15:47:35 +0100

    Agents' cycles are abstracted using Strategy pattern (fixes #16)

---
+++ aldeias/Assets/Scripts/Agents/HabitantDeliberative.cs
@@ -0,0 +1,13 @@
+using System;
+public class HabitantDeliberative : AgentImplementation {
+    private Habitant habitant;
+
+    public Action doAction() {
+        return null;
+    }
+
+    public HabitantDeliberative (Habitant habitant) {
+        this.habitant = habitant;
+    }
+}
+
